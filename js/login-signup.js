3963c27a4002216f182c1d73ac57659f1b7921e6
Amr mohamed <Amrmohammed1934@outlook.com>
2020-03-05 22:12:47 +0200

    Update login-signup.js

--- js/login-signup.js
+++ js/login-signup.js
@@ -5,12 +5,12 @@ var openSignUpButton = document.getElementById("slide-left-button");
 var openSignInButton = document.getElementById("slide-right-button");
 
 // The sidebars
-var leftText = document.getElementById("sign-in");
 var rightText = document.getElementById("sign-up");
+var leftText = document.getElementById("sign-in");
 
 // The forms
-var accountForm = document.getElementById("sign-in-info")
-var signinForm = document.getElementById("sign-up-info");
+var myAccountForm = document.getElementById("sign-up-info");
+var signUpForm = document.getElementById("sign-in-info");
 
 // Open the Sign Up page
 openSignUp = () =>{
@@ -19,20 +19,20 @@ openSignUp = () =>{
   overlay.classList.remove("open-sign-in");
   rightText.classList.remove("overlay-text-right-animation");
   // Add classes for animations
-  accountForm.className += " form-left-slide-out"
+  myAccountForm.className += " form-left-slide-out"
   rightText.className += " overlay-text-right-animation-out";
   overlay.className += " open-sign-up";
   leftText.className += " overlay-text-left-animation";
   // hide the sign up form once it is out of view
   setTimeout(function(){
-    accountForm.classList.remove("form-left-slide-in");
-    accountForm.style.display = "none";
-    accountForm.classList.remove("form-left-slide-out");
+    myAccountForm.classList.remove("form-left-slide-in");
+    myAccountForm.style.display = "none";
+    myAccountForm.classList.remove("form-left-slide-out");
   }, 700);
   // display the sign in form once the overlay begins moving right
   setTimeout(function(){
-    signinForm.style.display = "flex";
-    signinForm.classList += " form-right-slide-in";
+    signUpForm.style.display = "flex";
+    signUpForm.classList += " form-right-slide-in";
   }, 200);
 }
 
@@ -43,20 +43,20 @@ openSignIn = () =>{
   overlay.classList.remove("open-sign-up");
   rightText.classList.remove("overlay-text-right-animation-out");
   // Add classes for animations
-  signinForm.classList += " form-right-slide-out";
+  signUpForm.classList += " form-right-slide-out";
   leftText.className += " overlay-text-left-animation-out";
   overlay.className += " open-sign-in";
   rightText.className += " overlay-text-right-animation";
   // hide the sign in form once it is out of view
   setTimeout(function(){
-    signinForm.classList.remove("form-right-slide-in")
-    signinForm.style.display = "none";
-    signinForm.classList.remove("form-right-slide-out")
+    signUpForm.classList.remove("form-right-slide-in")
+    signUpForm.style.display = "none";
+    signUpForm.classList.remove("form-right-slide-out")
   },700);
   // display the sign up form once the overlay begins moving left
   setTimeout(function(){
-    accountForm.style.display = "flex";
-    accountForm.classList += " form-left-slide-in";
+    myAccountForm.style.display = "flex";
+    myAccountForm.classList += " form-left-slide-in";
   },200);
 }
 
